aa64cde34e951616141c794d30c1930755e5b04f
Md Faizy <mdgmfaizy@gmail.com>
2023-10-19 00:27:05 +0530

    update code

--- src/component/Count/Count.jsx
+++ src/component/Count/Count.jsx
@@ -1,24 +1,97 @@
+// import "./Count.css";
+
+// const Count = () => {
+//   return (
+//     <div>
+//       <article className="details1-count">
+//         <div className=" details-count">
+//           <div className="name">
+//             <h2>1232</h2>
+//             <p>Students</p>
+//           </div>
+//           <div className="name">
+//             <h2>64</h2>
+//             <p>Courses</p>
+//           </div>
+//           <div className="name">
+//             <h2>42</h2>
+//             <p>Events</p>
+//           </div>
+//           <div className="name">
+//             <h2>15</h2>
+//             <p>Trainers</p>
+//           </div>
+//         </div>
+//       </article>
+//     </div>
+//   );
+// };
+
+// export default Count;
+
+import { useEffect, useState } from "react";
 import "./Count.css";
 
 const Count = () => {
+  const [counts, setCounts] = useState({
+    students: 0,
+    courses: 0,
+    events: 0,
+    trainers: 0,
+  });
+
+  useEffect(() => {
+    const targetCounts = {
+      students: 1232,
+      courses: 64,
+      events: 44,
+      trainers: 16,
+    };
+    const duration = 3000;
+
+    const animateCount = (key, target) => {
+      const step = Math.ceil(target / (duration / 4));
+      let currentCount = 0;
+      const interval = setInterval(() => {
+        if (currentCount + step >= target) {
+          setCounts((prevCounts) => ({
+            ...prevCounts,
+            [key]: target,
+          }));
+          clearInterval(interval);
+        } else {
+          setCounts((prevCounts) => ({
+            ...prevCounts,
+            [key]: currentCount + step,
+          }));
+          currentCount += step;
+        }
+      }, 6);
+    };
+
+    for (const [key, value] of Object.entries(targetCounts)) {
+      animateCount(key, value);
+    }
+  }, []);
+
   return (
     <div>
       <article className="details1-count">
-        <div className=" details-count">
+        <div className="details-count">
           <div className="name">
-            <h2>1232</h2>
+            <h2>{counts.students}</h2>
             <p>Students</p>
           </div>
           <div className="name">
-            <h2>64</h2>
+            <h2>{counts.courses}</h2>
             <p>Courses</p>
           </div>
           <div className="name">
-            <h2>42</h2>
+            <h2>{counts.events}</h2>
             <p>Events</p>
           </div>
           <div className="name">
-            <h2>15</h2>
+            <h2>{counts.trainers}</h2>
             <p>Trainers</p>
           </div>
         </div>
--- src/component/Dummy/AboutLink.jsx
+++ src/component/Dummy/AboutLink.jsx
@@ -1,21 +1,22 @@
 import { data } from "./data.jsx";
 import aboutImg from "../../assets/about .jpg";
 import { HiMiniCheck } from "react-icons/hi2";
-
+// import styles from './AboutLink.module.css';
+import './AboutLink.css';
 const AboutLink = () => {
   return (
-    <div className="about-main-section">
-       <div className="main-about">
+    <div className="about_main_section">
+       <div className="main_about">
       <div className="about">
         <div className="text">
           {data.map((item, index) => (
             <div key={item.id}>
               <h1>{item.title}</h1>
               <h4 className="hypertext">{item.h4}</h4>
-              <p className="text-icon">
+              <p className="text_icon">
                 {/* <span> */}
                 {(index == 2 || index == 3 || index == 4) && (
-                  <HiMiniCheck className="about-icon" />
+                  <HiMiniCheck className="about_icon" />
                 )}
                 {item.paragraph}
 
@@ -25,7 +26,7 @@ const AboutLink = () => {
           ))}
         </div>
 
-        <div className="image-right">
+        <div className="image_right">
           <img src={aboutImg} alt="" />
         </div>
       </div>
